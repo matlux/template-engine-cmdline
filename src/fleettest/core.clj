3a50e45ab7cac041e4c3bfd8fd5d4a7c708f3831
Mathieu Gauthron <mathieu@gauthron.com>
2014-01-06 18:35:02 +0000

    Update core.clj

--- src/fleettest/core.clj
+++ src/fleettest/core.clj
@@ -8,6 +8,14 @@
   [args filename]
   `(fleet ~args (slurp (str "./" ~filename ".fleet")) {:escaping :xml}))
 
+(defn download-binary [to from]
+  (with-open [in  (io/input-stream (io/as-url from))]
+    (io/copy in (File. to))))
+
+(defn search-replace [sub-map file] (let [k (keys sub-map)
+	   replace-one (fn [[k v] line] (clojure.string/replace line (re-pattern k) v))
+	   replace-all (fn [sub-map line] (str (reduce #(replace-one %2 %1) line sub-map) "\n"))]
+	(apply str (map #(replace-all sub-map %) (read-file file)))))
 
 (defn write-file
   "Writes a value to a file"
@@ -21,9 +29,29 @@
     (reduce conj [] (line-seq rdr))))
 
 
+(defn instantiate-template-from-file
+  "instantiate a Fleet template."
+  [^String templatefile ^String instancefile ^String envfile ^String env]
+  (let [sub-map (read-string (apply str (read-file envfile)))]
+    (println sub-map)
+    (write-file ((fleet [? env] (slurp (str templatefile)) {:escaping :xml}) sub-map env) instancefile)))
+                    
+(defn instantiate-template
+  "instantiate a Fleet template."
+  [^String templatefile ^String instancefile ^java.util.Map sub-map]
+    (println sub-map)
+    (write-file ((fleet [?] (slurp (str templatefile)) {:escaping :xml}) (into {} sub-map)) instancefile))
 
+;; example config file
+;; {
+;; :groovyhome "/opt/groovy-2.1.7"
+;; :javahome "/opt/java/jdk1.7.0_09"
+;; }
 
-
+;; (require '[clojure.data.json :as json])
+;;(defn retrieve-jira-msg [jira]
+;;    (let [body (json/read-str (:body (retrieve-jira-memoized jira)))]
+;;		(-> body (get "fields") (get "summary"))))
 
 
 (defn -main
